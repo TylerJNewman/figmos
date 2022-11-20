54fd95049025b3e05a8d15489730dc76b37362b8
Tyler Newman <tylerjnewman18@gmail.com>
2022-11-19 21:03:57 -0800

    Try next-themes

---
+++ components/ThemeChanger.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+
+const themes = [
+	{ name: "Light" },
+	{ name: "Dark" },
+	// { name: "Emerald" },
+	// { name: "Pink" },
+];
+
+const ThemeChanger = () => {
+	const [mounted, setMounted] = useState(false);
+	const { theme, setTheme } = useTheme();
+
+	// When mounted on client, now we can show the UI
+	useEffect(() => setMounted(true), []);
+
+	if (!mounted) {
+		return null;
+	}
+
+	return (
+		<div className="p-8 flex justify-between items-center font-bold text-xl ">
+			<div>
+				<label htmlFor="theme-select" className="sr-only mr-2">
+					Choose theme:
+				</label>
+				<select
+					name="theme"
+					id="theme-select"
+					className="bg-white text-gray-800 border-gray-800 border py-1 px-3"
+					onChange={(e) => setTheme(e.currentTarget.value)}
+					value={theme}
+				>
+					<option value="">Select Theme</option>
+					{themes.map((t) => (
+						<option key={t.name.toLowerCase()} value={t.name.toLowerCase()}>
+							{t.name}
+						</option>
+					))}
+				</select>
+			</div>
+		</div>
+	);
+};
+
+export default ThemeChanger;
